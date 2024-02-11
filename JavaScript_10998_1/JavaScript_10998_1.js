5f6ec70c84fabfe790100a4ec4bb949f558e3244
SEYEON-PARK <62700321+SEYEON-PARK@users.noreply.github.com>
2024-02-11 20:44:01 +0900

    JavaScript언어 BAEKJOON 10998번 문제 풀기!(1)

--- JavaScript_10998_1/JavaScript_10998_1.js
+++ JavaScript_10998_1/JavaScript_10998_1.js
@@ -5,3 +5,10 @@
 
 첫째 줄에 A×B를 출력한다.
 */
+
+const fs = require('fs');
+const input = fs.readFileSync('/dev/stdin').toString().split(' '); // 공백을 기준으로 잘라서 배열을 만들어 input에 저장
+const A = parseInt(input[0]); // input[0]을 정수형으로 바꿔서 A에 저장
+const B = parseInt(input[1]);
+
+console.log(A*B);
